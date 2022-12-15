630506a8575b454d94de7392bff2ee1ef8207549
lahlaili <mohamedlahlaili246@gmail.com>
2022-12-15 16:14:59 +0100

    standar file content / just a test operation

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<title>login</title>
+</head>
+<body>
+	<center>
+		<h1>login</h1>
+	</center>
+	<?php 
+	$x=14;
+		echo "hello";
+		if ($x==1) {
+			echo "helooooo";
+		}else{
+			echo "not";
+		}
+	?>
+</body>
+</html>
